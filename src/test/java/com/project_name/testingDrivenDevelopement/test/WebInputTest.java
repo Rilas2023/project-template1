44cc117b7966c1caeab752c45c356dfe4ef69974
abdel <abdelazizouberka415@gmail.com>
2024-03-24 02:32:02 -0400

    update multiple steps

--- src/test/java/com/project_name/testingDrivenDevelopement/test/WebInputTest.java
+++ src/test/java/com/project_name/testingDrivenDevelopement/test/WebInputTest.java
@@ -1,14 +1,9 @@
-package com.project_name.tdd.test;
+package com.project_name.testingDrivenDevelopement.test;
 
-import com.project_name.tdd.pom.WebInputPom;
 import com.project_name.utilities.ConfigurationReader;
 import com.project_name.utilities.Driver;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class WebInputTest {
 
