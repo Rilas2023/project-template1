d2004b349b0b1d9fc586710f429b92e9b751a71a
abdel <abdelazizouberka415@gmail.com>
2024-03-24 02:45:42 -0400

    create local date object in POM

--- src/test/java/com/project_name/testingDrivenDevelopement/pom/WebInputPom.java
+++ src/test/java/com/project_name/testingDrivenDevelopement/pom/WebInputPom.java
@@ -5,18 +5,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class WebInputPom {
-  public static final String numbers="123456";
-  public static final String Text="text";
-  public static final String password="123456";
-  public static final String date="01/05/2024";
+    public static final String numbers = "123456";
+    public static final String Text = "text";
+    public static final String password = "123456";
+    LocalDate currentDate = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    public  String formattedDate = currentDate.format(formatter);
 
 
-
-    public WebInputPom(){
+    public WebInputPom() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy(xpath ="//a[normalize-space()='Web inputs']" )
+
+    @FindBy(xpath = "//a[normalize-space()='Web inputs']")
     public WebElement webInputButton;
 
     @FindBy(xpath = "//input[@id='input-number']")
@@ -31,6 +37,9 @@ public class WebInputPom {
     @FindBy(xpath = "//input[@id='input-date']")
     public WebElement inputDate;
 
-@FindBy(xpath = "//button[@id='btn-display-inputs']")
+    @FindBy(xpath = "//button[@id='btn-display-inputs']")
     public WebElement displayButton;
+
+    @FindBy(xpath = "//input[@class='input-box']")
+    public List<WebElement> actualInputData;
 }
