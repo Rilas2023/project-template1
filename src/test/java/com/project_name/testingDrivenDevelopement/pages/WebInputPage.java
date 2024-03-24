c3e120e224c0dab88d5e28ae6b8d53366b34fdc0
abdel <abdelazizouberka415@gmail.com>
2024-03-24 03:23:18 -0400

    seperate BDD and TDD

--- src/test/java/com/project_name/testingDrivenDevelopement/pages/WebInputPage.java
+++ src/test/java/com/project_name/testingDrivenDevelopement/pages/WebInputPage.java
@@ -1,6 +1,12 @@
 package com.project_name.testingDrivenDevelopement.pages;
 
-import com.project_name.testingDrivenDevelopement.fils.WebInputXML;
+import com.project_name.testingDrivenDevelopement.pom.WebInputPom;
+import com.project_name.utilities.BrowserUtils;
+import com.project_name.utilities.ConfigurationReader;
+import com.project_name.utilities.Driver;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -8,35 +14,37 @@ import java.util.stream.Collectors;
 
 public class WebInputPage {
 
-    private static WebInputXML webInputPom = new WebInputXML();
+   private static WebInputPom webInputPom =new WebInputPom();
 
 //    @Before
 //    public void navigateToHomePage() {
 //        Driver.getDriver().get(ConfigurationReader.getProperty("urlPractice"));
 //    }
 
-    public static boolean verifyInputDataAgainstOutputData() {
+@Test
+    public static boolean verifyInputDataAgainstOutputData(){
 
         webInputPom.webInputButton.click();
-
-        webInputPom.inputNumber.sendKeys(WebInputXML.numbers);
-        webInputPom.inputText.sendKeys(WebInputXML.Text);
-        webInputPom.inputPassword.sendKeys(WebInputXML.password);
+        webInputPom.inputNumber.sendKeys(WebInputPom.numbers);
+        webInputPom.inputText.sendKeys(WebInputPom.Text);
+        webInputPom.inputPassword.sendKeys(WebInputPom.password);
         webInputPom.inputDate.sendKeys(webInputPom.formattedDate);
         webInputPom.displayButton.click();
 
-        List<WebElement> actualInputData = webInputPom.actualInputData;
+    List<WebElement> actualInputData = webInputPom.actualInputData;
+    List<String> inputList = actualInputData.stream()
+            .map(element -> element.getAttribute("value"))
+            .collect(Collectors.toList());
+    System.out.println("inputList = " + inputList);
 
-        List<String> inputList = actualInputData.stream()
-                .map(element -> element.getAttribute("value"))
-                .collect(Collectors.toList());
+    List<WebElement> outputInputData = webInputPom.outputInputData;
+    List<String> outputList = outputInputData.stream()
+                    .map(WebElement::getText)
+                            .collect(Collectors.toList());
+    System.out.println("outputList = " + outputList);
 
-        List<WebElement> outputInputData = webInputPom.outputInputData;
 
-        List<String> outputList = outputInputData.stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
-        System.out.println("outputList = " + outputList);
-        return inputList.equals(outputList);
+
+return inputList.equals(outputList);
     }
 }
