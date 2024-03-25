eea925291f441bdbd74ce15be9d830f0a891c884
Rilas2023 <said.koudil@gmail.com>
2024-03-24 23:26:25 -0500

    navigating to dynamic Table

--- src/test/java/com/project_name/testingDrivenDevelopement/test/WebInputTest.java
+++ src/test/java/com/project_name/testingDrivenDevelopement/test/WebInputTest.java
@@ -1,5 +1,6 @@
 package com.project_name.testingDrivenDevelopement.test;
 
+import com.project_name.testingDrivenDevelopement.pages.DynamicTablePage;
 import com.project_name.testingDrivenDevelopement.pages.NotificationMessagePage;
 import com.project_name.testingDrivenDevelopement.pages.WebInputPage;
 import com.project_name.utilities.BrowserUtils;
@@ -15,24 +16,37 @@ public class WebInputTest {
 
 
     @Before
-    public void navigateToHomePage(){
+    public void navigateToHomePage() {
         Driver.getDriver().get(ConfigurationReader.getProperty("urlPractice"));
     }
 
     @Test
-    public void webInputVerification()  {
+    public void webInputVerification() {
 
         Assert.assertTrue(WebInputPage.verifyInputDataAgainstOutputData());
     }
 
     @Test
-    public void notificationMessageVerificationA(){
-
-        Assert.assertTrue(NotificationMessagePage.notificationMessageVerification());
+    public void notificationMessageVerificationA() {
+        try {
+            Assert.assertTrue(NotificationMessagePage.notificationMessageVerification());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
+    public void dynamicTableURLAssertion() {
+        Assert.assertTrue(DynamicTablePage.dynamicTableAccess());
+    }
+
+    @Test
+    public void memoryAvgOfInternetExplorer() throws InterruptedException {
+        Thread.sleep(3000);
+        System.out.println("DynamicTablePage.memory_avg() = " + DynamicTablePage.memory_avg());
+    }
     @After
-    public  void tearDown(){
+    public void tearDown() {
         Driver.closeDriver();
     }
 }
