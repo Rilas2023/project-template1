19276e201f266077af30872af300a7c4c121f7fa
abdel <abdelazizouberka415@gmail.com>
2024-03-24 21:43:49 -0400

    update utilities

--- src/test/java/com/project_name/testingDrivenDevelopement/fils/NotificationMessageXML.java
+++ src/test/java/com/project_name/testingDrivenDevelopement/fils/NotificationMessageXML.java
@@ -1,16 +1,23 @@
 package com.project_name.testingDrivenDevelopement.fils;
 
 import com.project_name.utilities.Driver;
+import org.junit.runner.manipulation.Ordering;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 public class NotificationMessageXML {
 
     public static final String expectedUnsuccessfulMessage = "Action unsuccessful, please try again";
 
     public NotificationMessageXML(){
 
+=======
+public class NotificationMessage {
+
+    public static final String expectedUnsuccessfulMessage="Action unsuccessful, please try again";
+
+    public NotificationMessage(){
+>>>>>>> 9d4a9fc (update utilities)
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
