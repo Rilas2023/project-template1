9fcfd771c5d636df0a4f735ca228732e2b43606b
abdel <abdelazizouberka415@gmail.com>
2025-04-27 16:01:51 -0400

    check existance

---
+++ src/test/java/com/project_name/behaviorDrivenDevelopment/pages/CheckExistance.java
@@ -0,0 +1,9 @@
+package com.project_name.behaviorDrivenDevelopment.pages;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface CheckExistance {
+}
--- src/test/java/com/project_name/behaviorDrivenDevelopment/pages/LogoutPage.java
+++ src/test/java/com/project_name/behaviorDrivenDevelopment/pages/LogoutPage.java
@@ -1,4 +1,12 @@
 package com.project_name.behaviorDrivenDevelopment.pages;
 
+
+@CheckExistance
 public class LogoutPage {
+
+    // add new behavior
+    // add new behavior5
+    // add new behavior second behaviour
+
+
 }
